cc992fbb954494d1789ac16cc7d74dfbf6ac190d
sbucki <sebastian.bucki@future-processing.com>
2022-11-04 11:01:39 +0100

    init server

---
+++ server.js
@@ -0,0 +1,13 @@
+const { response } = require('express');
+let express = require('express')
+
+let app = express();
+    
+app.use(express.static(__dirname+'/dist/heroku-angular'))
+
+app.get('/*', (req,resp)=>{
+    response.sendFile(__dirname+'/dist/heroku-angular/index.html')
+})
+
+
+app.listen(process.env.PORT || 8080);
